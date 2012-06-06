92954aeb3af525a003ebef095539c6a15c842810
Chris Jaquet <chrisjaquet@gmail.com>
2012-06-06 22:01:31 +0200

    Updated the eclipse project files for the cmake build systems. Added some gui files for the MainWindow.

--- src/Main.cpp
+++ src/Main.cpp
@@ -6,12 +6,30 @@
  * \version 0.1
  * \note    Copyright (c) 2012 Chris Jaquet
  **************************************************************************************************/
-
+// Local Headers ///////////////////////////////////////////////////////////////////////////////////
 #include "Main.h"
-#include <cstdio>
+#include "ui/MainWindow.h"
 
+// Global Headers //////////////////////////////////////////////////////////////////////////////////
+#include <cstdio>
+#include <QtGui>
+#include <QApplication>
+
+/**
+ * Program entry point
+ *
+ * \param argc  The argument count
+ * \param argv  The list of arguments passed by the operating system
+ *
+ * \return An error value. 0 is success.
+ */
 int main(int argc, char **argv)
 {
-    printf("OpenGMS says hello to the world.");
-    return 0;
+    printf("[main] OpenGMS says greetings to the world.");
+
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+
+    return a.exec();
 }
