4dada6fd92f8fa09c588bbbdeadf0060498b33b7
cjaquet <cjaquet@ee428258.stb.sun.ac.za>
2012-11-04 22:53:46 +0200

    Removed superfluous svn scripts

--- updateSVNRevision.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-rem Makes use of Tortoise SVN's SubWCRev to get the working copy's version 
-rem number.
-rem If the app is not available, copy the Version.in file to Version.h and 
-rem modify as necessary.
-SubWCRev %CD% src/Version.in src/Version.h
--- updateSVNRevision.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Makes use of the local app svnwcrev (the linux equivalent of Tortoise SVN's 
-# SubWCRev) to get the working copy's version number.
-#
-# If the app does not exist, copy the Version.in file to Version.h and modify as
-# necessary.
-./svnwcrev ${PWD} src/Version.in src/Version.h
